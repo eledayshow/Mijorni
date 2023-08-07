4f09aaa9f76ae89c336a1252bfe085b9cf9cc34e
Leonid <99201260+eledayshow@users.noreply.github.com>
2023-08-07 10:53:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ from defs import *
 
 weights = np.load('weights.npy')
 
-test_input = np.array([1, 0, 1, 0, 8, 4, 9, 9, 2])
-output_activation = softmax(np.dot(test_input, weights))
-
-print(np.round(output_activation[1]))
-
+mxm = .9
 m = 0
 i = 0
-while m < .9:
+while m < mxm:
     code = []
     for e in range(4): code.append(random.randint(0, 1))
     for e in range(4, 9): code.append(random.randint(0, 9))
